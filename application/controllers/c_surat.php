77ff23a10dd9fd0c1daf4833f1ab9b2aa63f3fcd
Sita Maryam Qodzarin <sitamaryamqodzarin001@gmail.com>
2019-05-21 00:07:41 +0700

    lanjutan pemilihan data warga dari pengajuan surat

--- application/controllers/c_surat.php
+++ application/controllers/c_surat.php
@@ -17,18 +17,32 @@ class c_surat extends CI_Controller {
 					'head'=>'v_header',
 					'foot'=>'v_footer',
 					'navbar'=>'v_navbar',
-					'slideshow'=>$this->m_user->getslideshow(),
 					'surat' => $this->m_user->surat_fasilitas_offline()
 				);
 
-				$this->load->view('surat/pengajuan-offline/v_fasilitas',$data);
+				
 			}
 			
 		}
 
+		public function pengajuan_offline(){
+			$data = array(
+					'head'=>'v_header',
+					'foot'=>'v_footer',
+					'navbar'=>'v_navbar',
+					'surat' => $this->m_user->surat_fasilitas_offline()
+				);
+
+			$this->load->view('surat/pengajuan-offline/v_fasilitas',$data);
+		} 
+
 		public function surat() {
-			$id_surat = $this->uri->segment(3);
-			$data['surat'] = $this->m_user->getSurat($id_surat);
+			$data = array(
+					'head'=>'v_header',
+					'foot'=>'v_footer',
+					'navbar'=>'v_navbar',
+					'surat' => $this->m_user->cari_surat()
+				);
 			$this->load->view('surat/v_surat',$data);
 		}
 }
--- application/models/m_user.php
+++ application/models/m_user.php
@@ -31,6 +31,13 @@
 				return $query->result();
 		}
 
+		public function cari_surat() {
+			$query = $this->db->select("*")
+					-> from ('warga')
+					-> get();
+				return $query->result();
+		}
+
 
 
 		public function getslideshow() {
