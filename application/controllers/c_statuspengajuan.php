94d02bea2fec2de717846f44ca2d96fa97308383
Husaini <48036941+husain234@users.noreply.github.com>
2019-05-22 10:08:24 +0700

    cek statuspengajuan

---
+++ application/controllers/c_statuspengajuan.php
@@ -0,0 +1,21 @@
+<?php 
+
+class c_statuspengajuan extends CI_Controller{
+	
+	function __construct() {
+			parent::__construct();
+			$this->load->model('m_user');
+		}
+
+	public function index()
+	{
+		$data = array(
+			'head'=>'v_header',
+			'foot'=>'v_footer',
+			'navbar'=>'v_navbar'
+		);
+
+		$this->load->view('warga/v_statuspengajuan',$data);
+	}
+}
+?>
