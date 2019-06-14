86190b693838f6e7328ed8e648045c197d76c3e2
Husaini <48036941+husain234@users.noreply.github.com>
2019-06-14 19:56:03 +0700

    list surat pengajuan

--- application/views/warga/v_listsurat.php
+++ application/views/warga/v_listsurat.php
@@ -19,6 +19,52 @@
 
       <?php $this->load->view($navbar); ?>
 
+      <!-- isi -->
+    <div class="container" id="isi">
+      <div class="row">
+        <div class="col-sm-12">
+          
+          <div class="judul">
+            <br>
+            <h1 align="center">Surat pengajuan</h1>
+            <br>
+          </div>
+          
+
+          <div class="list">
+          <form>
+              <table class="table table-bordered" align="center" style="width: 900px;">
+                  <thead>
+                      <tr style="text-align: center; font-size: 20px;">
+                      <th>Kode Surat </th>
+                      <th>Nama surat yang tersedia untuk pengajuan online</th>
+                      </tr>
+                  </thead>
+                     <?php foreach ($surat as $tampilkan) {
+                     ?>
+                  <tbody>
+                      <tr>
+                        <td style="text-align: center;"> <?php echo $tampilkan->kode_surat ?> </td>
+                        <td>
+                          <a href="<?php echo base_url().'c_surat/fromsuratonline/'.$tampilkan->kode_surat; ?>">
+                            <?php echo $tampilkan->nama_surat ?> 
+                          </a>
+                       </td>
+                      </tr>
+                  </tbody>
+                  <?php
+                    };
+                  ?>
+              </table>
+          </form>
+          </div>
+        
+        </div>
+      </div>
+    </div>
+
+    <!-- akhir isi -->
+
       
     <!-- footer -->
     <footer>
@@ -36,9 +82,9 @@
 
   <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script type="text/javascript" src="js/script.js"></script>
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="js/bootstrap.min.js"></script>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
