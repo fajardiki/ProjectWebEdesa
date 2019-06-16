e8a2d4a7d517bd6ea77b01e0b75581c885dbff88
Sita Maryam Qodzarin <sitamaryamqodzarin001@gmail.com>
2019-06-16 20:28:30 +0700

    update hutdesa

--- application/views/pengarsipan/v_hutdesa.php
+++ application/views/pengarsipan/v_hutdesa.php
@@ -2,8 +2,22 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
-hhyy
+	<div class="container">
+	<div align="center">
+	<?php
+		$images = glob("img_hut/*.*"); 
+        for ($i=0; $i<count($images); $i++) 
+        {
+        	$single_image = $images[$i];
+        	?>
+        	<img class="img-thumbnail" src="<?php echo $single_image; ?>" width="180" height="180">
+    <?php
+		}
+    ?>
+	</div>
+	</div>
 </body>
 </html>
