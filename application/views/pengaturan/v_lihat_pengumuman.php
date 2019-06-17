81599bde6482e605f24c3edf2c70fbca92cc394b
Husaini <48036941+husain234@users.noreply.github.com>
2019-06-17 16:47:30 +0700

    tampilan pengaturan

---
+++ application/views/pengaturan/v_lihat_pengumuman.php
@@ -0,0 +1,36 @@
+<div class="container">
+      <table width="100%" border="1px solid #000" class="table-bordered text-center mt-3">
+                <thead>
+                    <tr style="background-color: skyblue;">
+                        <th>Waktu</th>
+                        <th>Judul</th>
+                        <th>Isi</th>
+                        <th>Gambar</th>
+                        <th>Opsi</th>
+                    </tr>
+                </thead>
+                
+                <tbody>
+                    <tr>
+                        <td width="150">
+                            
+                            
+                        </td>
+                        <td width="150">
+                            
+                        </td>
+                        <td>
+                           
+                        </td>
+                        <td align="center" width="170">
+                            <img style="width: 150px;  " src="image_pengumuman">
+                        </td>
+                        <td align="center" width="70">
+                            <a href="delete_pengumuman.php?id_pengumuman" class="btn btn-info btn-sm">Delete</a>
+                        </td>
+
+                    </tr>
+                </tbody>
+                
+            </table>
+        </div>
