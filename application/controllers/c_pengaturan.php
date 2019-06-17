1bdaa50ba9a2d41e00ab2f7bd578409161ad6e3f
ahmadfaqihhamdani <47958650+ahmadfaqihhamdani@users.noreply.github.com>
2019-06-17 12:58:56 +0700

    masih eror

--- application/controllers/c_pengaturan.php
+++ application/controllers/c_pengaturan.php
@@ -7,6 +7,31 @@ class c_statuspengajuan extends CI_Controller{
 			$this->load->model('m_user');
 		}
 		public function index(){
+			if (!$this->session->userdata('username')) {
+				$this->load->view('v_login');
+			} else {
+				$data = array(
+					'head'=>'v_header',
+					'foot'=>'v_footer',
+					'navbar'=>'v_navbar',
+				);			
+			}
 			
 		}
+
+		public function editpengumuman(){
+			$data = array(
+					'head'=>'v_header',
+					'foot'=>'v_footer',
+					'navbar'=>'v_navbar',
+					'tittle'=>'edit pengumuman'
+					'side1' => 'v_editpengumuman'
+				);
+
+			$this->load->view('admin/v_editpengumuman',$data);
+		}
+
+			
+		
+		
 }
